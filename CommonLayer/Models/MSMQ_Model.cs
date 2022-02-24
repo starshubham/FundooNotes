1315295cbd2af18959f0578670d9702cd45ee3fa
Shubham Seth <shubhamsth1234@gmail.com>
2022-02-24 11:46:50 +0530

    Applied MSMQ to reset password

---
+++ CommonLayer/Models/MSMQ_Model.cs
@@ -0,0 +1,23 @@
+﻿using Experimental.System.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonLayer.Models
+{
+    public class MSMQ_Model
+    {
+        MessageQueue messageQueue = new MessageQueue();
+        public void MSMQSender()
+        {
+            messageQueue.Path = @".\private$\Token";//for windows path
+
+            if (!MessageQueue.Exists(messageQueue.Path))
+            {
+
+                MessageQueue.Create(messageQueue.Path);
+
+            }
+        }
+    }
+}
